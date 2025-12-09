935be5c3bf3e81dd642617a5db8b83d6daa1c92b
Alejandro Canadas <alejandrocanadas113@gmail.com>
2025-12-08 19:19:43 -0500

    first two queries (Top paying jobs and Top paying jobs skills) completed

---
+++ queries/TopPayingJobs.sql
@@ -0,0 +1,24 @@
+/*
+    Question: What are the top 20 highest paying jobs based on average salary?
+    Based on my needs: 
+        -I want this jobs to be for all types of Data analysis positions.
+        -I want this jobs to be remote.
+        -I want to know which companies are offering these jobs.
+*/
+
+SELECT
+    jpf.job_title AS job_role,
+    cd.name AS company_name,
+    jpf.salary_year_avg AS average_salary,
+    RANK() OVER (ORDER BY salary_year_avg DESC) AS rank_position
+FROM job_postings_fact AS jpf
+LEFT JOIN company_dim AS cd ON cd.company_id = jpf.company_id
+WHERE
+    (jpf.job_title_short LIKE '%Data Analyst%' AND jpf.job_work_from_home = TRUE)
+    AND
+    (jpf.salary_year_avg IS NOT NULL)
+LIMIT 20;
+
+
+
+
---
+++ queries/TopPayingJobsSkills.sql
@@ -0,0 +1,36 @@
+/*
+Question: What skills are required for the jobs located in the top 20?
+    - Add all the skills required for each of the jobs. 
+    - If they are null, do not display them.
+*/
+WITH top_20_paying_jobs AS(
+    SELECT 
+        jpf.job_id,
+        jpf.job_title AS job_role,
+        cd.name AS company_name,
+        jpf.salary_year_avg AS average_salary,
+        RANK() OVER (
+            ORDER BY salary_year_avg DESC
+        ) AS rank_position
+    FROM job_postings_fact AS jpf
+        LEFT JOIN company_dim AS cd ON cd.company_id = jpf.company_id
+    WHERE (
+            jpf.job_title_short LIKE '%Data Analyst%'
+            AND jpf.job_work_from_home = TRUE
+        )
+        AND (jpf.salary_year_avg IS NOT NULL)
+    LIMIT 20
+)
+
+SELECT 
+    sd.skills AS skill_name,
+    tpj.company_name,
+    tpj.average_salary
+FROM top_20_paying_jobs AS tpj
+JOIN 
+    skills_job_dim AS sjd ON tpj.job_id = sjd.job_id
+JOIN 
+    skills_dim AS sd ON sjd.skill_id = sd.skill_id
+ORDER BY tpj.rank_position ASC;
+
+
